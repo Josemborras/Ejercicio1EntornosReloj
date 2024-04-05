ac77e6dfef2b6c35cfe4bc759c6b98b993e4354a
dam1 <dam1@Mac-Mini-13.local>
2024-04-05 09:30:55 +0200

    datos David

--- src/Reloj.java
+++ src/Reloj.java
@@ -1,3 +1,39 @@
 public class Reloj {
-    
+    private int hora, minuto, segundo;
+
+    public Reloj(int hora, int minuto, int segundo) {
+        this.hora = hora;
+        this.minuto = minuto;
+        this.segundo = segundo;
+    }
+
+    public Reloj() {
+        this.hora = 0;
+        this.minuto = 0;
+        this.segundo = 0;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public int getSegundo() {
+        return segundo;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public void setMinuto(int minuto) {
+        this.minuto = minuto;
+    }
+
+    public void setSegundo(int segundo) {
+        this.segundo = segundo;
+    }
 }
