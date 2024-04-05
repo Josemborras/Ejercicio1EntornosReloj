9561de5c83f6e87e4f5ff89129cd3203f29d6ee4
Josemborras <josemanuel.borras@escuelaestech.es>
2024-04-05 09:19:51 +0200

    datos

--- src/Reloj.java
+++ src/Reloj.java
@@ -1,3 +1,24 @@
 public class Reloj {
+    String horaFalsa;
+    String minFalso;
+    String segFalso;
     
+    public String getHoraFalsa() {
+        return horaFalsa;
+    }
+    public void setHoraFalsa(String horaFalsa) {
+        this.horaFalsa = horaFalsa;
+    }
+    public String getMinFalso() {
+        return minFalso;
+    }
+    public void setMinFalso(String minFalso) {
+        this.minFalso = minFalso;
+    }
+    public String getSegFalso() {
+        return segFalso;
+    }
+    public void setSegFalso(String segFalso) {
+        this.segFalso = segFalso;
+    }
 }
