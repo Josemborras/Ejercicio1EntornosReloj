7749aec9f360c727c3082ac76a1da9eee5959579
marcosrodriguez <marcos.rodriguez@escuelaestech.es>
2024-04-05 09:21:16 +0200

    Marcos

--- src/Reloj.java
+++ src/Reloj.java
@@ -1,3 +1,26 @@
 public class Reloj {
+    int horas;
+    String minutos;
+    boolean tarde = true;
+
     
+    public int getHoras() {
+        return horas;
+    }
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+    public String getMinutos() {
+        return minutos;
+    }
+    public void setMinutos(String minutos) {
+        this.minutos = minutos;
+    }
+    public boolean isTarde() {
+        return tarde;
+    }
+    public void setTarde(boolean tarde) {
+        this.tarde = tarde;
+    }
 }
+
